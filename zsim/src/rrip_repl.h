cb4abf8e29a5338a1edd4ec3f9cdcab531fa8f60
Aditya Kovilur <aditya@Adityas-MacBook-Pro.local>
2025-11-04 20:23:49 -0600

    added SRRIP

--- zsim/src/rrip_repl.h
+++ zsim/src/rrip_repl.h
@@ -5,12 +5,39 @@
 
 // Static RRIP
 class SRRIPReplPolicy : public ReplPolicy {
-    protected:
-        // add class member variables here
+protected:
+  uint32_t *array;
+  uint32_t numLines;
+  uint32_t rpvMax;
 
-    public:
-        // add member methods here, refer to repl_policies.h
+public:
+  explicit SRRIPReplPolicy(uint32_t _numLines, uint32_t _rpvMax)
+      : numLines(_numLines), rpvMax(_rpvMax) {
+    array = gm_calloc<uint32_t>(numLines);
+  }
 
-        // DECL_RANK_BINDINGS;
+  ~SRRIPReplPolicy() { gm_free(array); }
+
+  void inline update(uint32_t id, const MemReq *req) { array[id] = 0; }
+
+  void replaced(uint32_t id) {
+    // No-op because nothing is required in this case
+  }
+
+  template <typename C> inline uint32_t rank(const MemReq *req, C cands) {
+    for (uint32_t i = 0; i < rpvMax; i++) {
+      for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
+        if (array[*ci] == rpvMax) {
+          return *ci;
+        }
+      }
+
+      for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
+        array[*ci]++;
+      }
+    }
+  }
+
+  DECL_RANK_BINDINGS;
 };
 #endif // RRIP_REPL_H_
